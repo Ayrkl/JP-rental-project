c53097d859b2c4470a381b52ff2e70480717527c
Ayrkl <cansinyrkl@gmail.com>
2026-04-19 02:16:12 +0300

    feat: add Zod schemas and types for user login and registration validation

--- common/src/schemas/auth.schema.ts
+++ common/src/schemas/auth.schema.ts
@@ -10,7 +10,11 @@ export type LoginInput = z.infer<typeof LoginSchema>;
 export const RegisterSchema = z.object({
   name: z.string().min(3, "Adınız en az 3 karakter olmalıdır."),
   email: z.string().email("Geçerli bir email adresi giriniz."),
-  password: z.string().min(6, "Şifreniz en az 6 karakter olmalıdır."),
+  password: z.string()
+    .min(8, "Şifreniz en az 8 karakter olmalıdır.")
+    .regex(/[A-Z]/, "Şifreniz en az bir büyük harf içermelidir.")
+    .regex(/[a-z]/, "Şifreniz en az bir küçük harf içermelidir.")
+    .regex(/[!@#$%^&*(),.?":{}|<>]/, "Şifreniz en az bir özel karakter içermelidir."),
   role: z.enum(['tenant', 'admin'])
 });
 
